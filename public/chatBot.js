9fc6fbe3efe791f5f61a561ec79a9d7fd0d0cb4b
Vishal Pandey <vishalpandey2332@gmail.com>
2026-06-11 12:21:38 +0530

    Update chatBot.js

--- public/chatBot.js
+++ public/chatBot.js
@@ -1,7 +1,7 @@
 console.log("Chatbot loaded");
 (function () {
 
-    const api_Url = "http://localhost:3000/api/chat";
+    const api_Url = "https://ai-customer-support-six-phi.vercel.app/api/chat";
 
     const scriptTag = document.currentScript;
 
